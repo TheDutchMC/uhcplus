e19b9faa65c2aadf8d5c13537f3b120ca4a6b31e
TheDutchMC04 <tobias64a@gmail.com>
2020-04-24 15:06:59 +0200

    Basics
    - Setup Gradle and added Spigot.
    - Added basic methods for a Spigot plugin

--- src/nl/thedutchmc/uhcplus/UhcPlus.java
+++ src/nl/thedutchmc/uhcplus/UhcPlus.java
@@ -1,5 +1,16 @@
 package nl.thedutchmc.uhcplus;
 
-public class UhcPlus {
+import org.bukkit.plugin.java.JavaPlugin;
 
+public class UhcPlus extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
 }
