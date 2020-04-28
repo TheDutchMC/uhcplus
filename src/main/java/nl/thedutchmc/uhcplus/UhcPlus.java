07608b14bcee847a7fcba0e92006623c66745d95
TheDutchMC <tobias64a@gmail.com>
2020-04-28 13:06:04 +0200

    Fixed version number
    - 0.1-BETA to 1.0-BETA

--- src/main/java/nl/thedutchmc/uhcplus/UhcPlus.java
+++ src/main/java/nl/thedutchmc/uhcplus/UhcPlus.java
@@ -11,7 +11,7 @@ import nl.thedutchmc.uhcplus.world.ChunkGenerator;
 
 public class UhcPlus extends JavaPlugin {
 
-	public static String VERSION = "0.1-BETA";
+	public static String VERSION = "1.0-BETA";
 		
 	@Override
 	public void onEnable() {
